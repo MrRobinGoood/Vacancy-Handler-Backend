06eb9a6ecc4ca604c9bfe9b60b03aada89cc5a20
Andrey Bartenev <59123372+MrRobinGoood@users.noreply.github.com>
2023-06-24 20:43:15 +0400

    Create two methods for api

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Body, status, UploadFile, File
 from fastapi.responses import JSONResponse, FileResponse
 from text_handler import get_result_dict, write_to_excel
 import pandas as pd
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 
@@ -15,13 +15,29 @@ class Resp:
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload")
 def upload_file(file: UploadFile = File(...)):
+    print('started upload')
     df = pd.read_excel(file.file.read())
     file.file.close()
     rd = get_result_dict(df['responsibilities(Должностные обязанности)'].tolist())
+    print(rd)
     write_to_excel(rd)
+    # file_path = 'resources/result.xlsx'
+    # return FileResponse(path=file_path, filename=file_path)
+
+@app.get("/download")
+def download_file():
     file_path = 'resources/result.xlsx'
     return FileResponse(path=file_path, filename=file_path)
 
